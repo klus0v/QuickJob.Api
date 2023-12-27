c70e4f60526f559c9c5549b6f3abdcdea53ed528
Klusov <klusov@skbkontur.ru>
2023-12-27 21:32:30 +0500

    migration

---
+++ QuickJob.DataModel/Migrations/20231225174132_server_v1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickJob.DataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class server_v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
