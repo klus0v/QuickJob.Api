685fa6dff4bb8c4de589b2145d90d25ef6368da4
Klusov <klusov@skbkontur.ru>
2023-12-27 23:21:06 +0500

    update models

--- QuickJob.DataModel/Migrations/20231227181631_server_v2.Designer.cs
+++ QuickJob.DataModel/Migrations/20231227181631_server_v2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using QuickJob.DataModel.Postgres;
@@ -12,9 +13,11 @@ using QuickJob.DataModel.Postgres;
 namespace QuickJob.DataModel.Migrations
 {
     [DbContext(typeof(QuickJobContext))]
-    partial class QuickJobContextModelSnapshot : ModelSnapshot
+    [Migration("20231227181631_server_v2")]
+    partial class server_v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ QuickJob.DataModel/Migrations/20231227181631_server_v2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickJob.DataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class server_v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDateTime",
+                table: "responses",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EditDateTime",
+                table: "responses",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateDateTime",
+                table: "responses");
+
+            migrationBuilder.DropColumn(
+                name: "EditDateTime",
+                table: "responses");
+        }
+    }
+}
