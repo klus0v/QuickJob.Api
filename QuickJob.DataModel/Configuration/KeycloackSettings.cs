25563d342756bbe997dfed4ad8eaf7701e46b610
Klusov <klusov@skbkontur.ru>
2024-01-12 18:22:39 +0500

    clean up

--- QuickJob.DataModel/Configuration/KeycloackSettings.cs
+++ QuickJob.DataModel/Configuration/KeycloackSettings.cs
@@ -2,16 +2,6 @@
 
 public class KeycloackSettings
 {
-    public string BaseUrl { get; set; }
-    public string RealmName { get; set; }
-    
-    public string SubClaim { get; set; }
-    public string AuthUrl { get; set; }
     public string Authority { get; set; }
     public string Audience { get; set; }
-
-    public string ClientId { get; set; }
-    public string ClientSecret { get; set; }
-    public string Login { get; set; }
-    public string Password { get; set; }
 }
