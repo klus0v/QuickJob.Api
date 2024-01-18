caada1b6ed4653cf2e32cfdb31a2c38a2c47f206
Klusov <klusov@skbkontur.ru>
2024-01-19 00:34:45 +0500

    log.Debug 2

--- QuickJob.Api/Middlewares/UserAuthMiddleware.cs
+++ QuickJob.Api/Middlewares/UserAuthMiddleware.cs
@@ -1,10 +1,19 @@
+using System.Security.Claims;
 using QuickJob.BusinessLogic.Extensions;
 using QuickJob.DataModel.Context;
+using Vostok.Logging.Abstractions;
 
 namespace QuickJob.Api.Middlewares;
 
 internal sealed class UserAuthMiddleware :  IMiddleware
-{ 
+{
+    private readonly ILog log;
+
+    public UserAuthMiddleware(ILog log)
+    {
+        this.log = log;
+    }
+
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         CommonContext.Initialize();
@@ -17,6 +26,11 @@ internal sealed class UserAuthMiddleware :  IMiddleware
             await next.Invoke(context);
             return;
         }
+        
+        log.Debug("___1 " + context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name)?.Value);
+        log.Debug("___2 " + context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value);
+        log.Debug("___3 " + context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.GivenName)?.Value);
+        log.Debug("___4 " + context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Surname)?.Value);
         
         RequestContext.ClientInfo.IsUserAuthenticated = true;
         RequestContext.ClientInfo.UserId = userId.Value;
